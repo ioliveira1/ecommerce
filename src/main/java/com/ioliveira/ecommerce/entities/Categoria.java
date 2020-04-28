528738780d63db1f817dae0650b09907c238746b
Igor Oliveira <igorfoliveira@gmail.com>
2020-04-28 00:40:46 -0300

    adicionado banco h2, dependencias jpa e criacao da entity Categoria

--- src/main/java/com/ioliveira/ecommerce/entities/Categoria.java
+++ src/main/java/com/ioliveira/ecommerce/entities/Categoria.java
@@ -3,12 +3,19 @@ package com.ioliveira.ecommerce.entities;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @EqualsAndHashCode(of = {"id"})
 @ToString
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
