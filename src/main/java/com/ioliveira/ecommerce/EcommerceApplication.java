72782c62b7d1a872aeb5ffd6503d5a5ce1230881
Igor Oliveira <igorfoliveira@gmail.com>
2020-05-05 16:17:56 -0300

    adicao de massa de dados de produtos e categorias

--- src/main/java/com/ioliveira/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ioliveira/ecommerce/EcommerceApplication.java
@@ -49,25 +49,41 @@ public class EcommerceApplication implements CommandLineRunner {
         Categoria cat5 = new Categoria("Jardinagem");
         Categoria cat6 = new Categoria("Decoração");
         Categoria cat7 = new Categoria("Perfumaria");
-        Categoria cat8 = new Categoria("Categoria 8");
-        Categoria cat9 = new Categoria("Categoria 9");
-        Categoria cat10 = new Categoria("Categoria 10");
-        Categoria cat11 = new Categoria("Categoria 11");
-        Categoria cat12 = new Categoria("Categoria 12");
 
         Produto p1 = new Produto("Computador", 2000.00);
         Produto p2 = new Produto("Impressora", 800.00);
         Produto p3 = new Produto("Mouse", 80.00);
+        Produto p4 = new Produto("Mesa de escritório", 300.00);
+        Produto p5 = new Produto("Toalha", 50.00);
+        Produto p6 = new Produto("Colcha", 200.00);
+        Produto p7 = new Produto("TV true color", 1200.00);
+        Produto p8 = new Produto("Roçadeira", 800.00);
+        Produto p9 = new Produto("Abajour", 100.00);
+        Produto p10 = new Produto("Pendente", 180.00);
+        Produto p11 = new Produto("Shampoo", 90.00);
 
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-        cat2.getProdutos().addAll(Collections.singletonList(p2));
+        cat2.getProdutos().addAll(Arrays.asList(p2, p4));
+        cat3.getProdutos().addAll(Arrays.asList(p5, p6));
+        cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
+        cat5.getProdutos().addAll(Collections.singletonList(p8));
+        cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+        cat7.getProdutos().addAll(Collections.singletonList(p11));
 
-        p1.getCategorias().addAll(Collections.singletonList(cat1));
-        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-        p3.getCategorias().addAll(Collections.singletonList(cat1));
+        p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
+        p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
+        p4.getCategorias().addAll(Collections.singletonList(cat2));
+        p5.getCategorias().addAll(Collections.singletonList(cat3));
+        p6.getCategorias().addAll(Collections.singletonList(cat3));
+        p7.getCategorias().addAll(Collections.singletonList(cat4));
+        p8.getCategorias().addAll(Collections.singletonList(cat5));
+        p9.getCategorias().addAll(Collections.singletonList(cat6));
+        p10.getCategorias().addAll(Collections.singletonList(cat6));
+        p11.getCategorias().addAll(Collections.singletonList(cat7));
 
-        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12));
-        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 
         Estado est1 = new Estado("Minas Gerais");
         Estado est2 = new Estado("São Paulo");
