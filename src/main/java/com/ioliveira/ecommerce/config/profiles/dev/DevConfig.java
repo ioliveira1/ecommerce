d24c115de3a3dfd3731ddf0164b0afb9c2f5be57
Igor Oliveira <igorfoliveira@gmail.com>
2020-05-08 19:08:25 -0300

    profile dev criado

---
+++ src/main/java/com/ioliveira/ecommerce/config/profiles/dev/DevConfig.java
@@ -0,0 +1,28 @@
+package com.ioliveira.ecommerce.config.profiles.dev;
+
+import com.ioliveira.ecommerce.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDataBase() {
+        if (strategy.equals("create")) {
+            dbService.instantiateTestDataBase();
+        }
+        return true;
+    }
+
+}
